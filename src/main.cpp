e4b876978da9235c9604a68ed8843c897b6b1473
Akash6222 <akashdubey25274@gmail.com>
2026-06-25 23:37:23 +0530

    Fixed "Welcome to ALG" showing up in gnome aopp title

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    app.setApplicationName("Welcome to ALG");
+    // app.setApplicationName("Welcome to ALG");
     app.setApplicationVersion(ALG_WELCOME_VERSION);
     app.setDesktopFileName("alg-welcome");
     app.setOrganizationName("Arch Linux GUI");
